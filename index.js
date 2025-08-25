31cd7b8b23013e1419da4972c69479bab3bae1d0
FabricioCP1806 <fabriciojcorper2@gmail.com>
2025-08-24 22:15:46 -0500

    create index.js file

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res)=>{
+    res.send("Hello World");
+});
+
+app.listen(PORT,()=>{
+    console.log(`La aplicacion de Express esta corriendo en el Puerto https://localHost:${PORT}`);
+});
+
+
+
+
